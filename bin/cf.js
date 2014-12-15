1e492c0b9622594e38a5dd787aabadf0089b9f43
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2014-12-14 20:23:19 -0500

    Testing with bin

--- bin/cf.js
+++ bin/cf.js
@@ -3,10 +3,5 @@
 
 process.title = 'cf.js';
 
-var cfjs = require('../app.js');
-cfjs.start(function (err) {
-  if (!err) {
-    jitsu.log.info('Cf.js'.grey + ' ok'.green.bold);
-  }
-  process.exit(err ? 1 : 0);
-});
+require('../app.js');
+
